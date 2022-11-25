22490714cc8643d3470126527e22d03bb3a688d8
Ana Lozano Moreno <analozanomor@gmail.com>
2022-11-25 11:13:13 +0100

    add handleLetters function

--- src/components/App.js
+++ src/components/App.js
@@ -4,34 +4,47 @@ import { useState } from 'react';
 function App() {
   const [numberOfErrors, setNumberOfErrors] = useState(0);
   const [lastLetter, setLastLetter] = useState([]);
-  const [word, setWord] = useState ('katakroker');
-  const [userLetters, setUserLetters] = useState ([]);
-
+  const [word, setWord] = useState('katakroker');
+  const [userLetters, setUserLetters] = useState([]);
 
   const handleClickBtn = (ev) => {
     setNumberOfErrors(numberOfErrors + 1);
   };
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
 
   const handleLetter = (ev) => {
-    const regularExpresion = /[a-zA-ZáÁéÉíÍóÓúÚüÜ]/;
+    let regularExpresion = /[a-zA-ZáÁéÉíÍóÓúÚüÜ´]/;
     if (regularExpresion.test(ev.target.value)) {
       setLastLetter(ev.target.value);
+      setUserLetters([...userLetters, ev.target.value]);
     } else {
       setLastLetter('');
     }
-
+  };
+  const renderErrorLetters = () => {
+    const errorLetters = userLetters.filter(
+      (letter) =>
+        word.toLocaleLowerCase().includes(letter.toLocaleLowerCase()) === false
+    );
+    return errorLetters.map((letter, index) => {
+      return (
+        <li key={index} className='letter'>
+          {letter}
+        </li>
+      );
+    });
   };
 
-  /*const exists = userLetters.includes(letter.toLocaleLowerCase());
-  return (
-    <li key={index} className='letter'>
-      {exists ? letter : ''}
-    </li>}*/
   const renderSolutionLetters = () => {
     const wordLetters = word.split('');
     return wordLetters.map((letter, index) => {
       const exists = userLetters.includes(letter.toLocaleLowerCase());
-      return(<li key ={index} className='letter'>{}</li>
+      return (
+        <li key={index} className='letter'>
+          {exists ? letter : ''}
+        </li>
       );
     });
   };
@@ -45,26 +58,17 @@ function App() {
         <section>
           <div className='solution'>
             <h2 className='title'>Solución:</h2>
-            <ul className='letters'>
-            {renderSolutionLetters()}
-            </ul>
+            <ul className='letters'>{renderSolutionLetters()}</ul>
           </div>
           <div className='error'>
             <h2 className='title'>Letras falladas:</h2>
-            <ul className='letters'>
-              <li className='letter'>f</li>
-              <li className='letter'>q</li>
-              <li className='letter'>h</li>
-              <li className='letter'>p</li>
-              <li className='letter'>x</li>
-            </ul>
+            <ul className='letters'>{renderErrorLetters()}</ul>
           </div>
-          <form className='form'>
+          <form className='form' onSubmit={handleSubmit}>
             <label className='title' htmlFor='last-letter'>
               Escribe una letra:
             </label>
             <input
-        
               className='form__input'
               maxLength='1'
               type='text'
